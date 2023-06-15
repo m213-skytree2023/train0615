64da7b5ee320e69aed5c970050a8784dfb7bcc20
m213 <>
2023-06-15 15:21:18 +0900

    ll 15.21

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from pic_class import PicInfo
 
 app = Flask(__name__)
 
@@ -12,8 +13,10 @@ def toppage():
 def graph_search():
     if request.method == 'POST':
         search = request.form.get("search")
-        #graph_list=
-    return render_template('index.html')
+        graph_list= []
+        g = PicInfo("111111", "picture_type", "categories", "preview_url", "https://pixabay.com/get/gd52d02f4eb1ba4c4126e040589756f0913796caee666a72ce9f8697cc35965ec75a337f1e9dec6c852ac94f6f955f16607f6dc703b4c381275752d3ce79f2b1b_640.jpg", 123, 456)
+        graph_list.append(g)
+    return render_template('searchResult.html',search = search, graph_list = graph_list)
 
 
 
